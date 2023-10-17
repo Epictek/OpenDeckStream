bb9d3ca95ca32081c03680a2e01fae3f5605cef3
Kieran Coldron <kieran@coldron.com>
2023-10-18 00:33:37 +0100

    Update entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,10 +3,13 @@ set -e
 
 export DOTNET_ROOT=/usr/share/dotnet
 export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
+export DOTNET_CLI_HOME="/tmp/DOTNET_CLI_HOME"
+export DOTNET_CLI_TELEMETRY_OPTOUT=1
 
+mkdir -p /backend/out
 cd /backend/src/obs_recorder
 
-dotnet publish -r linux-x64 -c Release -o /backend/out/
+dotnet publish -r linux-x64 -c Release -o /backend/out/ 
 
 mkdir -p /backend/out/obs
 mv /obs-portable/* /backend/out/obs/
